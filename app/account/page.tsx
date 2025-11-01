9e4d010604f89871f4baa009c67cfffb43ffe44a
Ahmed Khorkhash <khorkhash3@gmail.com>
2025-11-01 14:16:31 +0200

    Feat: Add Account pages and core navigation/interaction components

---
+++ app/account/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Guest Area",
+};
+
+export default function Account() {
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Ahmed
+    </h2>
+  );
+}
